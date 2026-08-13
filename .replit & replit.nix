e4ece6475cdf6a235a8eb1e728804d9c79221b46
Mayass Ali <157918175+mibo01699@users.noreply.github.com>
2026-08-14 02:04:49 +0300

    Create .replit & replit.nix

---
+++ .replit & replit.nix
@@ -0,0 +1,8 @@
+# أخبر Replit بكيفية تشغيل خادمك ومحرك المقاصة فوراً بمجرد الضغط على زر Run
+run = "node server/app.js"
+
+[nix]
+channel = "stable-23_05"
+
+[deployment]
+run = "node server/app.js"
