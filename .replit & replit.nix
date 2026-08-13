ee8e12b3c456c0df8ba2f7c239de596d2f06721b
Mayass Ali <157918175+mibo01699@users.noreply.github.com>
2026-08-14 02:07:03 +0300

    Update replit & replit.nix

--- .replit & replit.nix
+++ .replit & replit.nix
@@ -6,3 +6,12 @@ channel = "stable-23_05"
 
 [deployment]
 run = "node server/app.js"
+
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-18_x      # تثبيت الـ Node.js لتشغيل خادم المقاصة والاستدعاء المتوالي
+    pkgs.python310        # تثبيت البايثون لتشغيل خوارزمية الأرقام الصحيحة الصارمة لـ BIGISH-YER
+    pkgs.nodePackages.npm
+  ];
+}
+
