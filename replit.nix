cbc5fa1555eb8ffcdffc2df54660bfe51d1e7930
Mayass Ali <157918175+mibo01699@users.noreply.github.com>
2026-08-19 22:04:24 +0300

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs_20
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.rustc
-    pkgs.cargo
-    pkgs.cargo-audit
+    pkgs.nodejs-18_x
+    pkgs.python310
   ];
 }
